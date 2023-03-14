5ecfc8bac296217e920aec3503d3f227bb989126
Роман Иванов <zaregi@mail.ru>
2023-03-15 00:00:35 +0300

    Models for Title_Genre_Category

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
+class Title(models.Model):
+    name = models.TextField()
+    year = models.IntegerField()
+    rating = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreTitle',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='titles',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        related_name='genres',
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='titles',
+    )
+
+    def __str__(self):
+        return f'{self.genre} {self.title}'
