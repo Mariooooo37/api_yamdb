8607d8d188b4828e475a19bb3751b68a5220c0a5
Anatolii Fedchenko <av.fedchenko@gmail.com>
2023-03-16 13:25:49 +0600

    Add models, view, endpoints for Reviews and Comments

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,11 +2,22 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from reviews.views import CategoryViewSet, GenreViewSet, TitleViewSet
+from reviews.views import ReviewViewSet, CommentViewSet
 
 v1_router = DefaultRouter()
 v1_router.register('categories', CategoryViewSet, basename='categories')
 v1_router.register('genres', GenreViewSet, basename='genres')
 v1_router.register('titles', TitleViewSet, basename='titles')
+v1_router.register(
+    r'titles/(?P<title_id>[\d]+)/reviews',
+    ReviewViewSet,
+    basename='reviews',
+)
+v1_router.register(
+    r'titles/(?P<title_id>[\d]+)/reviews/(?P<review_id>[\d]+)/comments',
+    CommentViewSet,
+    basename='comments',
+)
 
 app_name = 'api'
 
--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets, mixins
+from django.shortcuts import get_object_or_404
 
 from reviews.models import Category, Genre, Title
 from reviews.serializers import (
     CategorySerializer, GenreSerializer, TitleSerializer
 )
+from reviews.serializers import ReviewSerializer, CommentSerializer
 
 
 class CategoryViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
@@ -28,3 +30,40 @@ class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
     # Пермишены
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(
+            title=title,
+            text=self.request.data['text'],
+            author=self.request.user,
+            score=self.request.data['score'],
+        )
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Title, id=review_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Title, id=review_id)
+        serializer.save(
+            review=review,
+            text=self.request.data['text'],
+            author=self.request.user,
+        )
