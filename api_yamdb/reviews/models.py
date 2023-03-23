ffc07f52174797c095d16584fb6ab31050fc8f1e
Владимир Якушев <le-2006@yandex.ru>
2023-03-23 16:27:31 +0100

    review1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,62 +1,64 @@
-from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from user.models import User
 
-class User(AbstractUser):
-    '''Роли пользователей'''
-    ADMIN = 'admin'
-    MODERATOR = 'moderator'
-    USER = 'user'
-    ROLES = [
-        (ADMIN, 'Administrator'),
-        (MODERATOR, 'Moderator'),
-        (USER, 'User'),
-    ]
-    email = models.EmailField(
-        max_length=254,
-        verbose_name='Адрес электронной почты',
-        unique=True,
-    )
-    username = models.CharField(
-        verbose_name='Имя пользователя',
-        max_length=150,
-        unique=True,
-    )
-    role = models.CharField(
-        verbose_name='Роль',
-        max_length=50,
-        choices=ROLES,
-        default=USER
-    )
-    '''Добавляем поле bio'''
-    bio = models.TextField(
-        verbose_name='О себе',
-        null=True,
-        blank=True
-    )
 
-    @property
-    def is_moderator(self):
-        return self.role == self.MODERATOR
+# class User(AbstractUser):
+#    '''Роли пользователей'''
+#    ADMIN = 'admin'
+#    MODERATOR = 'moderator'
+#    USER = 'user'
+#    ROLES = [
+#        (ADMIN, 'Administrator'),
+#        (MODERATOR, 'Moderator'),
+#        (USER, 'User'),
+#    ]
+#    email = models.EmailField(
+#        max_length=254,
+#        verbose_name='Адрес электронной почты',
+#        unique=True,
+#    )
+#    username = models.CharField(
+#        verbose_name='Имя пользователя',
+#        max_length=150,
+#        unique=True,
+#    )
+#    role = models.CharField(
+#        verbose_name='Роль',
+#        max_length=50,
+#        choices=ROLES,
+#        default=USER
+#    )
+#    '''Добавляем поле bio'''
+#    bio = models.TextField(
+#        verbose_name='О себе',
+#        null=True,
+#        blank=True
+#    )
 
-    @property
-    def is_admin(self):
-        return self.role == self.ADMIN
+#    @property
+#    def is_moderator(self):
+#        return self.role == self.MODERATOR
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    class Meta:
-        ordering = ['id']
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-
-        constraints = [
-            models.CheckConstraint(
-                check=~models.Q(username__iexact="me"),
-                name="username_is_not_me"
-            )
-        ]
+#    @property
+#    def is_admin(self):
+#        return self.role == self.ADMIN
+#
+#    USERNAME_FIELD = 'email'
+#    REQUIRED_FIELDS = ['username']
+#
+#    class Meta:
+#        ordering = ['id']
+#        verbose_name = 'Пользователь'
+#        verbose_name_plural = 'Пользователи'
+#
+#        constraints = [
+#            models.CheckConstraint(
+#                check=~models.Q(username__iexact="me"),
+#                name="username_is_not_me"
+#            )
+#        ]
 
 
 class Genre(models.Model):
